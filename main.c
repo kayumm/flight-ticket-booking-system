3fd8e777d6977fde3a0714574f2684fd4da125e9
kayumm <92858412+kayumm@users.noreply.github.com>
2023-08-28 20:20:40 +0600

    Update main.c

--- main.c
+++ main.c
@@ -4,3 +4,19 @@ int main() {
   printf("hello world");
   return 0;
 }
+
+
+void seating_data_collect_store() {
+	FILE *seating_data;
+	int seat_number;
+	printf("Enter the seat you want to book: ");
+	scanf("%d", &seat_number);
+	seating_data = fopen("seat_data.txt", "a");
+	if (seating_data == NULL) {
+		printf("Error! Could not find file.");
+	} else {
+		fprintf(seating_data, "%d ", seat_number);
+	}
+	fclose(seating_data);
+} 
+
